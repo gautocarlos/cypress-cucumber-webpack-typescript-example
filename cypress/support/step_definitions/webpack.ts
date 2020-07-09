04e154ee869dbab39619c47db4b9267d558669cd
gautocarlos <gautocarlos@users.noreply.github.com>
2020-07-09 01:24:18 -0300

    First commit

--- cypress/support/step_definitions/webpack.ts
+++ cypress/support/step_definitions/webpack.ts
@@ -11,5 +11,8 @@ Given(`webpack is configured`, () => {});
 
 Then(`this test should work just fine!`, () => {
   myAssertion();
-  cy.visit('https://google.com')
+  cy.visit('https://www.playdigital.com.ar/faq/');
+  cy.get('div.flex:nth-child(2) > button:nth-child(1)').click();
+  cy.get('div.flex:nth-child(3) > button:nth-child(1)').click();
+  cy.get('div.flex:nth-child(4) > button:nth-child(1)').click();
 });
