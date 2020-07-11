3d27e6760b5f4b85e67058049ca30d28b5a3bfbf
gautocarlos <gautocarlos@users.noreply.github.com>
2020-07-10 21:01:49 -0300

    Cypress - allure reports
    Added allure reports suppport.
    Added chai for expect validations.
    Updated dependencies

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,8 +1,11 @@
-const webpack = require("@cypress/webpack-preprocessor");
+const AllureWriter = require('@shelex/cypress-allure-plugin/writer');
+const webpack = require('@cypress/webpack-preprocessor');
 
-module.exports = on => {
+module.exports = (on, config) => {
   const options = {
-    webpackOptions: require("../webpack.config.js")
+    webpackOptions: require('../webpack.config.js')
   };
-  on("file:preprocessor", webpack(options));
+  on('file:preprocessor', webpack(options));
+  AllureWriter(on, config);
+  return config;
 };
